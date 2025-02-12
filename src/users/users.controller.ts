fed1dbef80ba849b8a5507f723a0b39addbcaaf3
airydev001 <airydev585@gmail.com>
2025-02-12 11:17:08 +0100

    how to declare params in nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Post,Param} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get('/:id')
+    public getUsers(@Param() params:any){
+        console.log(params)
+        return "You sent a get request to users endpoint";
+    }
+
+    @Post()
+    public createUsers(){
+        return "You sent a post to users endpoints "
+    }
+}
