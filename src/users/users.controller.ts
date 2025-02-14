1a975c22de6c5cba6599cfb726b2e802e0a8ffbf
airydev001 <airydev585@gmail.com>
2025-02-14 16:17:30 +0100

    using headers , Ip

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,19 @@
 /* eslint-disable prettier/prettier */
-import { Controller,Get,Post,Param,Query,Body,Req} from '@nestjs/common';
+import { Controller,Get,Post,Param,Query,Body,Req,Headers,Ip} from '@nestjs/common';
 import {Request} from 'express';
 @Controller('users')
 export class UsersController {
     @Get('/:id')
-    public getUsers(@Param() params:any, @Query() query:any){
-        console.log(params);
-        console.log(query);
+    public getUsers(@Param('id') id:any, @Query('limit') limit:any){
+        console.log(id);
+        console.log(limit);
         return "You sent a get request to users endpoint";
     }
 
     @Post()
-    public createUsers(@Req() request:Request){
-        console.log(request);
+    public createUsers(@Body('email') email:any, @Headers() headers,@Ip() ip:any){
+        console.log(headers);
+        console.log(ip);
         return "You sent a post to users endpoints "
     }
 }
