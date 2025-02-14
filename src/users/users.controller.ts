e5f84daece92ecb8ad5de15e5068b5534c34c058
airydev001 <airydev585@gmail.com>
2025-02-14 15:35:46 +0100

    using req and body

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller,Get,Post,Param} from '@nestjs/common';
-
+import { Controller,Get,Post,Param,Query,Body,Req} from '@nestjs/common';
+import {Request} from 'express';
 @Controller('users')
 export class UsersController {
     @Get('/:id')
-    public getUsers(@Param() params:any){
-        console.log(params)
+    public getUsers(@Param() params:any, @Query() query:any){
+        console.log(params);
+        console.log(query);
         return "You sent a get request to users endpoint";
     }
 
     @Post()
-    public createUsers(){
+    public createUsers(@Req() request:Request){
+        console.log(request);
         return "You sent a post to users endpoints "
     }
 }
