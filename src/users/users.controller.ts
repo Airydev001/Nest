1e5036d9599c4f4fa20f48e58ec143d731c78b8f
airydev001 <airydev585@gmail.com>
2025-02-14 17:06:08 +0100

    Learn about parseIntValue

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Controller,Get,Post,Param,Query,Body,Req,Headers,Ip} from '@nestjs/common';
+import { Controller,Get,Post,Param,Query,Body,Req,Headers,Ip,ParseIntPipe,DefaultValuePipe} from '@nestjs/common';
 import {Request} from 'express';
 @Controller('users')
 export class UsersController {
     @Get('/:id')
-    public getUsers(@Param('id') id:any, @Query('limit') limit:any){
-        console.log(id);
+    public getUsers(@Param('id', ParseIntPipe)
+     id:number | undefined, 
+     @Query('limit',new DefaultValuePipe(10), ParseIntPipe) limit:number,@Query('page', new DefaultValuePipe(1),
+         ParseIntPipe) page:number){
+        console.log(typeof id);
+        console.log(typeof page);
+        console.log(page);
         console.log(limit);
         return "You sent a get request to users endpoint";
     }
